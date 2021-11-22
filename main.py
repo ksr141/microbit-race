45ded0f41c9c058e1cf6b1c621e329fdeb984247
ksr141 <ksr141@gmail.com>
2021-11-22 09:14:13 -1000

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def DrawScreen():
     for Screen_X in range(5):
         if Screen_X != Player_X:
             led.unplot(Screen_X, 4)
-        if Road1[Screen_X]:
+        if Screen_X != Road1_Opening:
             led.unplot(Screen_X, Road1_Y - 1)
             led.plot_brightness(Screen_X, Road1_Y, 50)
-        if Road2[Screen_X]:
+        if Screen_X != Road2_Opening:
             led.unplot(Screen_X, Road2_Y - 1)
             led.plot_brightness(Screen_X, Road2_Y, 50)
 
@@ -18,25 +18,21 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def MakeNewRoad():
-    global RandomOpening, Road1, Road1_Y, Score, Road2, Road2_Y
+    global Road1_Opening, Road1_Y, Score, Road2_Opening, Road2_Y
     if Road1_Y == 4:
-        RandomOpening = randint(0, 4)
-        Road1 = [1, 1, 1, 1, 1]
-        Road1[RandomOpening] = 0
+        Road1_Opening = randint(0, 4)
         Road1_Y = 0
         for Screen_X2 in range(5):
-            if Road1[Screen_X2]:
+            if Screen_X2 != Road1_Opening:
                 led.plot_brightness(Screen_X2, Road1_Y, 50)
-        Score += 1
+        Score += 10
     if Road2_Y == 4:
-        RandomOpening = randint(0, 3)
-        Road2 = [1, 1, 1, 1, 1]
-        Road2[RandomOpening] = 0
+        Road2_Opening = randint(0, 4)
         Road2_Y = 0
         for Screen_X3 in range(5):
-            if Road2[Screen_X3]:
+            if Screen_X3 != Road2_Opening:
                 led.plot_brightness(Screen_X3, Road2_Y, 50)
-        Score += 1
+        Score += 10
 
 def on_button_pressed_b():
     global Player_X
@@ -46,14 +42,13 @@ def on_button_pressed_b():
         led.plot(Player_X, 4)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-RandomOpening = 0
 Road2_Y = 0
 Road1_Y = 0
 Player_X = 0
-Road2: List[number] = []
-Road1: List[number] = []
-Road1 = [1, 1, 1, 0, 1]
-Road2 = [1, 0, 1, 1, 1]
+Road2_Opening = 0
+Road1_Opening = 0
+Road1_Opening = randint(0, 4)
+Road2_Opening = randint(0, 4)
 Player_X = 2
 Road1_Y = 1
 Road2_Y = -1
@@ -65,10 +60,10 @@ def on_forever():
     Road1_Y = Road1_Y + 1
     Road2_Y = Road2_Y + 1
     DrawScreen()
-    if Road1_Y == 4 and Road1[Player_X] == 1:
+    if Road1_Y == 4 and Road1_Opening != Player_X:
         game.set_score(Score)
         game.game_over()
-    if Road2_Y == 4 and Road2[Player_X] == 1:
+    if Road2_Y == 4 and Road2_Opening != Player_X:
         game.set_score(Score)
         game.game_over()
     MakeNewRoad()
